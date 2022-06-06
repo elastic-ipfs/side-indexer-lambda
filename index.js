9dc4b014ecc41ca6cc72e99e37773e1c4515ed30
Alan Shaw <alan.shaw@protocol.ai>
2022-06-06 14:07:52 +0100

    test: add simple test

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 import { S3Client, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3'
 import { MultihashIndexSortedWriter } from 'cardex'
 import { CarIndexer } from '@ipld/car/indexer'
-
-const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env
+import { Readable } from 'stream'
 
 /**
  * @type {import('aws-lambda').Handler<import('aws-lambda').S3Event>}
  */
-export async function handler (event) {
+export async function handler (event, context) {
+  /** @type {typeof S3Client} */
+  const S3 = typeof context?.clientContext?.Custom?.S3Client === 'function'
+    ? context.clientContext.Custom.S3Client
+    : S3Client
   const records = event.Records
     .filter(r => r.eventName.startsWith('ObjectCreated'))
     .filter(r => r.s3.object.key.endsWith('.car'))
 
   for (const r of records) {
-    const s3 = new S3Client({
+    const s3 = new S3({
       region: r.awsRegion,
       credentials: {
-        accessKeyId: AWS_ACCESS_KEY_ID,
-        secretAccessKey: AWS_SECRET_ACCESS_KEY
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
       }
     })
 
@@ -30,7 +33,8 @@ export async function handler (event) {
 
     const { writer, out } = MultihashIndexSortedWriter.create()
     ;(async () => {
-      for (const blockIndexData of CarIndexer.fromIterable(res.Body)) {
+      const indexer = await CarIndexer.fromIterable(res.Body)
+      for await (const blockIndexData of indexer) {
         await writer.put(blockIndexData)
       }
       await writer.close()
@@ -39,7 +43,7 @@ export async function handler (event) {
     const putCmd = new PutObjectCommand({
       Bucket: r.s3.bucket,
       Key: r.s3.object.key + '.idx',
-      Body: out
+      Body: Readable.from(out)
     })
 
     await s3.send(putCmd)
---
+++ test/test.spec.js
@@ -0,0 +1,63 @@
+import anyTest from 'ava'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { MultihashIndexSortedReader } from 'cardex'
+import { concat, equals } from 'uint8arrays'
+import { GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3'
+import { CID } from 'multiformats'
+import * as raw from 'multiformats/codecs/raw'
+import { handler } from '../index.js'
+
+/** @typedef {{ mockS3: import('./helpers/mock-s3').MockS3 }} Context */
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+/** @type {import('ava').TestFn<Context>} */
+const test = anyTest
+
+test('creates an index on put', async t => {
+  const cid = CID.parse('bafybeihkqv2ukwgpgzkwsuz7whmvneztvxglkljbs3zosewgku2cfluvba')
+
+  /** @type {Uint8Array} */
+  let putData
+  class MockS3Client {
+    async send (cmd) {
+      if (cmd instanceof GetObjectCommand) {
+        const carPath = path.join(__dirname, 'fixtures', cmd.input.Key)
+        return { Body: fs.createReadStream(carPath) }
+      }
+      if (cmd instanceof PutObjectCommand) {
+        const chunks = []
+        for await (const chunk of cmd.input.Body) {
+          chunks.push(chunk)
+        }
+        putData = concat(chunks)
+        return
+      }
+      throw new Error('unexpected command')
+    }
+  }
+
+  await handler({
+    Records: [{
+      eventName: 'ObjectCreated:Put',
+      s3: {
+        bucket: 'test',
+        object: { key: `${cid}.car` }
+      }
+    }]
+  }, { clientContext: { Custom: { S3Client: MockS3Client } } })
+
+  // Read entire index to ensure a valid index was written.
+  // Sanity check at least the root block was indexed!
+  const reader = MultihashIndexSortedReader.fromBytes(putData)
+  let isRootBlockIndexed = false
+  for await (const entry of reader.entries()) {
+    if (equals(entry.digest, cid.multihash.digest)) {
+      isRootBlockIndexed = true
+    }
+    console.log(`${CID.createV1(raw.code, entry.multihash)}, @ ${entry.offset}`)
+  }
+  t.true(isRootBlockIndexed, 'root CID indexed')
+})
